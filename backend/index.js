0fe4169bee24dec9351e87b6a45892e3e1cfbb1a
William <powilliam19@gmail.com>
2020-01-14 16:10:43 -0400

    Add MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = express.json()
 
 const app = express()
+
+mongoose.connect(
+    'mongodb+srv://powilliam:powilliam@heaven-uc9c7.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    err => {
+        if (err) {
+            console.error(err)
+
+            throw err
+        } else {
+            console.log('> Connection with MongoDB successfully')
+        }
+    }
+)
+
 app.use(bodyParser)
 
 // HTTP methods -> ['GET', 'POST', 'PUT', 'DELETE']
