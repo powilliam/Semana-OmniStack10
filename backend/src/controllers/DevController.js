55cfe663c5f783c1f353b2eefaf005beb4bf13b8
William <powilliam19@gmail.com>
2020-01-14 17:30:14 -0400

    Add verification step to avoid duplicate devs register

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,23 +5,37 @@ class DevController {
     async store(request, response) {
         const { github, techs, latitude, longitude } = request.body
     
-        const { name = login, avatar_url, bio } = await getGithubUserData(github)
+        const registeredDev = await verifyIfDevIsAlreadyRegistered(github)
+
+        if (!registeredDev) {
+            console.log('> Dev isn`t registered')
+
+            const { name = login, avatar_url, bio } = await getGithubUserData(github)
     
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+        
+            const dev = await Devs.create({
+                name,
+                avatar: avatar_url,
+                techs: techs.split(',').map(techs => techs.trim()),
+                github,
+                bio,
+                location
+            })
+        
+            return response.json(dev)
+        } else {
+            console.log('> Dev is already registered')
+
+            return response.json(registeredDev)
         }
-    
-        const dev = await Devs.create({
-            name,
-            avatar: avatar_url,
-            techs: techs.split(',').map(techs => techs.trim()),
-            github,
-            bio,
-            location
-        })
-    
-        return response.json(dev)
+
+        async function verifyIfDevIsAlreadyRegistered(account) {
+            return await Devs.findOne({ github: account })
+        }
     
         async function getGithubUserData(account) {
             const response = await axios.get(
