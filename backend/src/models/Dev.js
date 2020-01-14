5742edb452b1924fd192fe4cbf3cbec95fce38d1
William <powilliam19@gmail.com>
2020-01-14 17:11:18 -0400

    Create a location field to store the geolocation provided by MongoDB

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
+const PointSchema = require('./utils/PointSchema')
 
 const DevSchema = new mongoose.Schema({
     name: String,
     github: String,
     bio: String,
     avatar: String,
-    techs: [String]
+    techs: [String],
+    location: {
+        type: PointSchema,
+        index: '2dsphere'
+    }
 })
 
 const DevModel = mongoose.model('Devs', DevSchema)
