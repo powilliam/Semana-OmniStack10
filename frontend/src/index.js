ed04e5cacb40d2a4252a4dddba2eae6370ce9120
William <powilliam19@gmail.com>
2020-01-15 14:09:33 -0400

    First reactjs commit

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// What React is: 
+// Component -> class/function that returns HTML tags
+// Props -> attributes in the Component
+// State -> information that will be manipulated by the component
