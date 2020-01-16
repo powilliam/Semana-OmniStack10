824af3aab4e32be47b02f9a66029f576e1d5a985
William <powilliam19@gmail.com>
2020-01-15 21:24:35 -0400

    Create buttons for update and delete Dev and function to Remove dev

--- frontend/src/components/DevList/index.js
+++ frontend/src/components/DevList/index.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import api from '../../services/api'
 
 import './styles.css'
 
 export default function DevList({ developerInformation }) {
-    return (
-        <li className="dev-item">
-            <header>
-              <img src={developerInformation.avatar} alt={developerInformation.name}/>
-              <div className="user-info">
-                <strong>{developerInformation.name}</strong>
-                <span>{developerInformation.techs.join(', ')}</span>
-              </div>
-            </header>
-            <p>{developerInformation.bio}</p>
-            <a href={`https://github.com/${developerInformation.github}`}>Acessar perfil do Github</a>
-        </li>
+  async function UnregisterDeveloper() {
+    await api.delete(`/devs/${developerInformation._id}/unregister`)
+  }
+
+  return (
+      <li className="dev-item">
+          <header>
+            <img src={developerInformation.avatar} alt={developerInformation.name}/>
+            <div className="user-info">
+              <strong>{developerInformation.name}</strong>
+              <span>{developerInformation.techs.join(', ')}</span>
+            </div>
+          </header>
+          <p>{developerInformation.bio}</p>
+          <a href={`https://github.com/${developerInformation.github}`}>Acessar perfil do Github</a>
+          <div className="dev-controll">
+            <button id="update-dev">Atualizar</button>
+            <button id="remove-dev" onClick={UnregisterDeveloper}>Remover</button>
+          </div>
+      </li>
     )
 }
