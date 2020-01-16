2f08e27a6ba21587c3830a7f87b67a15bb7c4775
William <powilliam19@gmail.com>
2020-01-16 11:05:45 -0400

    Add form input to search devs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, TextInput, Image, TouchableOpacity } from 'react-native'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import MapView, { Marker, Callout } from 'react-native-maps'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Main({ navigation }) {
     const [ currentRegion, setCurrentRegion ] = useState(null)
+    const [ techs, setTechs ] = useState('')
 
     useEffect(() => {
         async function loadStartLocation() {
@@ -39,23 +41,39 @@ export default function Main({ navigation }) {
             { !currentRegion ? (
                 <View />
             ) : (
-                <MapView 
-                    style={styles.map}  
-                    initialRegion={currentRegion}
-                >
-                    <Marker coordinate={{ latitude: -7.503922, longitude: -63.026543 }}>
-                        <Image 
-                            style={styles.avatar} 
-                            source={{ uri: 'https://avatars0.githubusercontent.com/u/55867831?s=460&v=4' }}
-                        />
+                <>
+                    <MapView 
+                        style={styles.map}  
+                        initialRegion={currentRegion}
+                    >
+                        <Marker coordinate={{ latitude: -7.503922, longitude: -63.026543 }}>
+                            <Image 
+                                style={styles.avatar} 
+                                source={{ uri: 'https://avatars0.githubusercontent.com/u/55867831?s=460&v=4' }}
+                            />
 
-                        <Callout onPress={navigateToGithubProfile} style={styles.callout}>
-                            <Text style={styles.devName}>William Porto</Text>
-                            <Text style={styles.devBio}>Chemistry student at UFAM. Ìn love with Node.js, ReactJS and React Native</Text>
-                            <Text style={styles.devTechs}>Node.js, React JS, React Native</Text>
-                        </Callout>
-                    </Marker>
-                </MapView>
+                            <Callout onPress={navigateToGithubProfile} style={styles.callout}>
+                                <Text style={styles.devName}>William Porto</Text>
+                                <Text style={styles.devBio}>Chemistry student at UFAM. Ìn love with Node.js, ReactJS and React Native</Text>
+                                <Text style={styles.devTechs}>Node.js, React JS, React Native</Text>
+                            </Callout>
+                        </Marker>
+                    </MapView>
+                    <View style={styles.searchForm}>
+                        <TextInput 
+                            style={styles.searchInput}
+                            placeholder="Buscar devs por tecnologias"
+                            placeholderTextColor="#999"
+                            autoCapitalize="words"
+                            autoCorrect={false}
+                        />
+                        <TouchableOpacity 
+                            style={styles.searchButton}
+                        >
+                            <MaterialIcons name="my-location" size={20} color="#FFF"/>
+                        </TouchableOpacity>
+                    </View>
+                </>
             ) }
         </>
     )
@@ -85,5 +103,38 @@ const styles = StyleSheet.create({
     },
     devTechs: {
         marginTop: 5
+    },
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2
+    },
+    searchButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     }
 })
