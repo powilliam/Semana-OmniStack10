9f2c6a78b0c5e0a0ad0b97dc177db7b069ed389d
William <powilliam19@gmail.com>
2020-01-16 09:52:15 -0400

    Create stack navigator

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello OmniStack</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'tomato',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
