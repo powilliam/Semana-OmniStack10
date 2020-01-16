d88919d199796f452298ee91a317e3aeac1e443c
William <powilliam19@gmail.com>
2020-01-16 09:24:54 -0400

    Initialize mobile folder

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello OmniStack</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'tomato',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
